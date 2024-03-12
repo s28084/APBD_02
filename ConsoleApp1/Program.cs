1c50fc3523d6823d2128da57107ea8c6bb06943f
Zuzanna Wypych <s28084@pjwstk.edu.pl>
2024-03-12 13:29:41 +0100

    Average

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,14 @@
 ﻿Console.WriteLine("Hello, User!");
+int[] arr = new[] { 1, 2, 3, 4, 5 };
+Console.WriteLine(GetAvg(arr));
 ;
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+    return (double)sum / arr.Length;
+}
